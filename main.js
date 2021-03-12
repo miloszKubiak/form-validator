596f7bb6fed2d542637e6b194af27d04518d9411
miloszKubiak <milypan69@gmail.com>
2021-03-12 10:23:47 +0100

    Added a function to clear the form

---
+++ main.js
@@ -0,0 +1,16 @@
+const userName = document.querySelector('#username');
+const pass = document.querySelector('#password');
+const pass2 = document.querySelector('#password2');
+const email = document.querySelector('#email');
+const sendBtn = document.querySelector('.send');
+const clearBtn = document.querySelector('.clear');
+const popup = document.querySelector('.popup');
+
+
+clearBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    [userName, pass, pass2, email].forEach(el => {
+        el.value = '';
+    });
+});
